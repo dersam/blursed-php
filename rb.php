675de48ed3704db342b71f6260bd8ff115bf7136
Sam Schmidt <sam.schmidt@shopify.com>
2026-09-11 11:41:16 -0400

    whitespace

--- rb.php
+++ rb.php
@@ -4,14 +4,14 @@ $name  = "Ada Lovelace";
 $role  = "Engineer";
 $langs = array("Ruby", "PHP", "Assembly");
 
-printf("Hello %s".PHP_EOL, $name);
-printf("Role: %s".PHP_EOL, $role);
+printf("Hello %s" . PHP_EOL, $name);
+printf("Role: %s" . PHP_EOL, $role);
 
 function greet($who) {
   return implode(" ", ["Sup,", $who]);
 }
 
-echo greet("world").PHP_EOL;
+echo greet("world") . PHP_EOL;
 
 $x = null;
 echo isset($x) ? "set" : "not";
